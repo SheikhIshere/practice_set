b2f5f94b4b3cbcbb544da85650fe0fb405b21154
SheikhIshere <thesheikh255@gmail.com>
2025-08-25 23:28:44 +0600

    creating seventh project

--- six/accounts/urls.py
+++ six/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
-    path('profile/', ProfileVeiw.as_view(), name='profile'),    
+    path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
 ]
+
---
+++ six/accounts/views.py
@@ -0,0 +1,72 @@
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.http import Http404
+from django.views.generic import CreateView, DetailView, TemplateView
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from .forms import SignupForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import ProfileModel
+
+# Create your views here.
+
+
+class SignupView(CreateView):
+    model = User
+    template_name = 'signup.html'
+    form_class = SignupForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('homepage')
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:profile', kwargs={'username': self.request.user.username})
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('accounts:login')
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    template_name = 'profile.html'
+    model = User
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    context_object_name = 'user_profile'
+
+    def get_object(self, queryset=None):
+        username = self.kwargs.get('username')
+        try:
+            return User.objects.select_related('profilemodel').get(username=username)
+        except User.DoesNotExist:
+            raise Http404("User does not exist")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        try:
+            context['profile'] = user.profilemodel
+        except ProfileModel.DoesNotExist:
+            # Create profile if it doesn't exist
+            context['profile'] = ProfileModel.objects.create(
+                user=user,
+                first_name=user.first_name,
+                last_name=user.last_name,
+                email=user.email,
+                mobile=''  # Set default or required fields
+            )
+        return context
+    
+    def dispatch(self, request, *args, **kwargs):
+        # Only allow users to view their own profile
+        if not request.user.is_authenticated or request.user.username != kwargs.get('username'):
+            return redirect('accounts:login')
+        return super().dispatch(request, *args, **kwargs)
+    
+    
+# ZebaFauzia372009_
